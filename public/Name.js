523913a81a85d295c9ffbb711d5cd7f74b059b04
Muhammad Samiullah <itzsamiullah1@gmail.com>
2023-07-10 13:45:15 +0500

    App.js, Hash.js, Name.js

---
+++ public/Name.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Name = () => {
+  return (
+    <div>Samiullah</div>
+  )
+}
+
+export default Name
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+// import Name from '../public/name';
+import Hash from './Hash';
 
 function App() {
   return (
@@ -7,7 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* <Name/> */}
+          {/* <Hash/> */}
+          My name is <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
